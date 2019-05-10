0177d9b4759f2e372720489585136d6f6ae03d1c
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2019-05-10 22:48:02 +0200

    start server on given port

--- server.go
+++ server.go
@@ -1,18 +1,24 @@
 package kurz
 
 import (
+	"fmt"
 	"net/http"
 )
 
 type server URLShortener
 
-func Start() {
+// Start listens for connections on given port
+func Start(port int) error {
 
 	localdb := newLocalDB()
 	shortener := newURLShortener(*localdb)
 	newServer := server(*shortener)
 
 	http.HandleFunc("/encode/", newServer.serveEncodeURL)
+	formattedPort := fmt.Sprintf(":%d", port)
+
+	err := http.ListenAndServe(formattedPort, nil)
+	return err
 }
 
 func (s *server) serveEncodeURL(w http.ResponseWriter, r *http.Request) {
